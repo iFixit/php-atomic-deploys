1745ff91af63c2d8471dd3babdb989df57fa8357
Daniel Beardsley <daniel@ifixit.com>
2016-09-28 11:07:37 -0700

    Tests: assert opcache works

--- tests/OpcacheTest.php
+++ tests/OpcacheTest.php
@@ -5,7 +5,9 @@ use iFixit\Opcache\Opcache;
 
 class OpcacheTest extends PHPUnit_Framework_TestCase {
    public function setup() {
+      ini_set('opcache.validate_timestamps', 1);
       ini_set('opcache.revalidate_freq', 100);
+      ini_set("opcache.file_update_protection", 0);
    }
 
    public function testIniSettingsArePresent() {
@@ -34,5 +36,28 @@ class OpcacheTest extends PHPUnit_Framework_TestCase {
    public function testIniSettingEnable() {
       ini_set('opcache.enable', 0);
       $op = new Opcache();
+   }
+
+   public function testInvalidation() {
+      $file = $this->temp();
+      $this->makePhpFile($file, 'a');
+      $this->assertSame('a', require $file);
+
+      // Sleep long enough for the clock to tick over one second so the
+      // timestamp changes.
+      usleep(1200*1000);
+
+      // Assert cache isn't cleared when the file is updated
+      $this->makePhpFile($file, 'b');
+      $this->assertSame('a', require $file);
+   }
+
+   protected function makePhpFile($file, $returnVal) {
+      $php = "<? return '$returnVal';";
+      file_put_contents($file, $php);
+   }
+
+   protected function temp() {
+      return tempnam(sys_get_temp_dir(), 'php');
    }
 }
